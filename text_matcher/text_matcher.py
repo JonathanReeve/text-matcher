8048fe5551e66866077d8b847abced371461ecf9
Jonathan Reeve <jon.reeve@gmail.com>
2016-10-22 16:19:21 -0400

    more debugging

--- text_matcher/text_matcher.py
+++ text_matcher/text_matcher.py
@@ -79,7 +79,7 @@ class Matcher:
         """ Looks up the passage in the original text, using its spans. """
         matchTokens = text.tokens[start:start+length]
         spans = text.spans[start:start+length]
-        logging.debug('Len(spans) =  %s; len(text) = %s' % (len(spans), len(text)) )
+        logging.debug('Len(spans) =  %s; len(text) = %s' % (len(spans), len(text.text)) )
         passage = text.text[spans[0][0]:spans[-1][-1]]
         return passage 
 
