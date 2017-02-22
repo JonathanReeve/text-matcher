c7483bbf6e541a0c707a76ea1987cc6a40ee2db8
Jonathan Reeve <jon.reeve@gmail.com>
2017-02-22 11:48:34 -0500

    heavy rewrite with fuzzy matching
    Fuzzy matching using Levenstein distances, healing matches, all kinds of things.

--- text_matcher/text_matcher.py
+++ text_matcher/text_matcher.py
@@ -1,136 +1,15 @@
 # coding: utf-8
 
-import nltk
-import re 
+import click
+from .matcher import Text, ExtendedMatch, Matcher
 import os
 import glob
 import csv
-import difflib 
 import logging
 import itertools
-from nltk.util import ngrams 
-from difflib import SequenceMatcher
-from string import punctuation
-from termcolor import colored
-import click
 
-class Text: 
-    def __init__(self, filename, removeStopwords=True): 
-        self.filename = filename
-        self.tokens = self.getTokens(removeStopwords)
-        self.trigrams = self.ngrams(3)
-        
-    @property
-    def text(self):
-        """ Reads the file in memory. """
-        f = open(self.filename, encoding='utf-8', errors='ignore')
-        return f.read() 
-
-    def getTokens(self, removeStopwords=True): 
-        """ Tokenizes the text, breaking it up into words, removing punctuation. """
-        tokenizer = nltk.RegexpTokenizer('[a-zA-Z]\w+\'?\w*') # A custom regex tokenizer. 
-        spans = list(tokenizer.span_tokenize(self.text))
-        # Take note of how many spans there are in the text
-        self.length = spans[-1][-1] 
-        tokens = tokenizer.tokenize(self.text)
-        tokens = [ token.lower() for token in tokens ] # make them lowercase
-        if not removeStopwords: 
-            self.spans = spans
-            return tokens
-        tokenSpans = list(zip(tokens, spans)) # zip it up
-        stopwords = nltk.corpus.stopwords.words('english') # get stopwords
-        tokenSpans = [ token for token in tokenSpans if token[0] not in stopwords ] # remove stopwords from zip
-        self.spans = [ x[1] for x in tokenSpans ] # unzip; get spans
-        return [ x[0] for x in tokenSpans ] # unzip; get tokens
-    
-    def ngrams(self, n): 
-        """ Returns ngrams for the text."""
-        return list(ngrams(self.tokens, n))
-
-class Matcher: 
-    def __init__(self, fileA, fileB, threshold, ngramSize, removeStopwords):
-        """
-        Gets the texts from the files, tokenizes them, 
-        cleans them up as necessary. 
-        """
-        self.threshold = threshold
-        self.ngramSize = ngramSize
-        
-        self.textA, self.textB = Text(fileA, removeStopwords=removeStopwords), \
-                Text(fileB, removeStopwords=removeStopwords)
-        
-        self.textAgrams = self.textA.ngrams(ngramSize)
-        self.textBgrams = self.textB.ngrams(ngramSize)
-
-        self.locationsA = []
-        self.locationsB = []
-
-    def getContext(self, text, start, length, context): 
-        match = self.getTokensText(text, start, length)
-        before = self.getTokensText(text, start-context, context)
-        after = self.getTokensText(text, start+length, context)
-        match = colored(match, 'red')
-        out = " ".join([before, match, after])
-        out = out.replace('\n', ' ') # Replace newlines with spaces. 
-        out = re.sub('\s+', ' ', out)
-        return out
-
-    def getTokensText(self, text, start, length):  
-        """ Looks up the passage in the original text, using its spans. """
-        matchTokens = text.tokens[start:start+length]
-        spans = text.spans[start:start+length]
-        if len(spans) == 0: 
-            # Don't try to get text or context beyond the end of a text. 
-            passage = ""
-        else: 
-            passage = text.text[spans[0][0]:spans[-1][-1]]
-        return passage 
-
-    def getLocations(self, text, start, length, asPercentages=False): 
-        """ Gets the numeric locations of the match. """
-        spans = text.spans[start:start+length]
-        if asPercentages: 
-            locations = (spans[0][0]/text.length, spans[-1][-1]/text.length)
-        else: 
-            locations = (spans[0][0], spans[-1][-1])
-        return locations
-
-    def getMatch(self, match, textA, textB, context): 
-        length = match.size + self.ngramSize - 1 # offset according to nGram size 
-        wordsA = self.getContext(textA, match.a, length, context)
-        wordsB = self.getContext(textB, match.b, length, context)
-        spansA = self.getLocations(textA, match.a, length)
-        spansB = self.getLocations(textB, match.b, length)
-        self.locationsA.append(spansA)
-        self.locationsB.append(spansB)
-        line1 = ('%s: %s %s' % (colored(textA.filename, 'green'), spansA, wordsA) )
-        line2 = ('%s: %s %s' % (colored(textB.filename, 'green'), spansB, wordsB) )
-        return line1 + '\n' + line2
-
-    def match(self): 
-        """
-        This does the main work of finding matching n-gram sequences between
-        the texts.
-        """
-        sequence = SequenceMatcher(None,self.textAgrams,self.textBgrams)
-        matchingBlocks = sequence.get_matching_blocks()
-
-        # Only return the matching sequences that are higher than the 
-        # threshold given by the user. 
-        highMatchingBlocks = [match for match in matchingBlocks if match.size > self.threshold]
-    
-        numBlocks = len(highMatchingBlocks)
-        self.numMatches = numBlocks
-        
-        if numBlocks > 0: 
-            print('%s total matches found.' % numBlocks, flush=True)
-
-        for num, match in enumerate(highMatchingBlocks): 
-            print('match: ', match)
-            out = self.getMatch(match, self.textA, self.textB, 5)
-            print('\n')
-            print('match %s:' % (num+1), flush=True)
-            print(out, flush=True)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def getFiles(path): 
     """ 
@@ -179,8 +58,10 @@ def createLog(logfile, columnLabels):
 @click.command()
 @click.argument('text1')
 @click.argument('text2') 
-@click.option('-t', '--threshold', type=int, default=2, help='The shortest length of match to include.')
-@click.option('-n', '--ngrams', type=int, default=3, help='The ngram n-value to match against.')
+@click.option('-t', '--threshold', type=int, default=2, \
+        help='The shortest length of match to include.')
+@click.option('-n', '--ngrams', type=int, default=3, \
+        help='The ngram n-value to match against.')
 @click.option('-l', '--logfile', default='log.txt', help='The name of the log file to write to.')
 @click.option('--stops', is_flag=True, help='Include stopwords in matching.', default=False)
 @click.option('--verbose', is_flag=True, help='Enable verbose mode, giving more information.')
@@ -208,6 +89,17 @@ def cli(text1, text2, threshold, ngrams, logfile, verbose, stops):
     logging.debug('Comparing %s pairs.' % numPairs)
     logging.debug('List of pairs to compare: %s' % pairs)
 
+    logging.debug('Loading files into memory.')
+
+    texts = {}
+    for filename in texts1+texts2: 
+        with open(filename) as f: 
+            text = f.read() 
+        if filename not in texts: 
+            texts[filename] = text
+
+    logging.debug('Loading complete.')
+
     for index, pair in enumerate(pairs): 
         logging.debug('Now comparing pair %s of %s.' % (index, numPairs))
         logging.debug('Comparing %s with %s.' % (pair[0], pair[1]))
@@ -215,11 +107,14 @@ def cli(text1, text2, threshold, ngrams, logfile, verbose, stops):
         # Make sure we haven't already done this pair. 
         inLog = checkLog(logfile, [pair[0], pair[1]])
 
-        # Set up columns and their labels. 
-        columnLabels = ['Text A', 'Text B', 'Threshold', 'N-Grams', 'Num Matches', 'Text A Length', 'Text B Length', 'Locations in A', 'Locations in B']
+        # FIXME: Ignore
+        inLog = False
+
 
         if inLog is None: 
-        # This means that there isn't a log file. Let's set one up.
+            # This means that there isn't a log file. Let's set one up.
+            # Set up columns and their labels. 
+            columnLabels = ['Text A', 'Text B', 'Threshold', 'N-Grams', 'Num Matches', 'Text A Length', 'Text B Length', 'Locations in A', 'Locations in B']
             logging.debug('No log file found. Setting one up.')
             createLog(logfile, columnLabels)
             
@@ -227,8 +122,15 @@ def cli(text1, text2, threshold, ngrams, logfile, verbose, stops):
             logging.debug('This pair is already in the log. Skipping.')
             continue
 
+        logging.debug('Processing texts.')
+
+        filenameA, filenameB = pair[0], pair[1]
+        textA, textB = texts[filenameA], texts[filenameB]
+        textObjA = Text(textA, filenameA)
+        textObjB = Text(textB, filenameB)
+
         # Do the matching. 
-        myMatch = Matcher(pair[0], pair[1], threshold, ngrams, stops)
+        myMatch = Matcher(textObjA, textObjB, threshold, ngrams, stops)
         myMatch.match()
 
         # Write to the log, but only if a match is found.
